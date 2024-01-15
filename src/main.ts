4a464eb969ba57922b4c649450401286050d0698
Alexandra Dernova <alexder@gmx.net>
2024-01-15 18:03:04 +0100

    cleaning

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,22 @@ let score: number = 0
 let scoreAny = "0"
 let userName: string = ""
 let isAdmin: boolean = false
-
+const list = [15, 6, 3213, 9, 0, 12, 8464 , 1, 1264, 481, 186, 1031, 194];
+// Удалите из списка два младших числа и четыре старших числа.
+// Удалить все числа, которые делятся на 4.
+// Сложите все числа.
+function myList(l:number[]){
+    const listSort=l.sort((a, b) => b - a)
+console.log(listSort)
+    const listSquare:number[] = listSort.map(number=>number*number)
+    console.log(listSquare)
+    const listRemoveFirst2andLast4: number[] =listSquare.slice(2, listSort.length - 4)
+console.log(listRemoveFirst2andLast4)
+    const listFilter:number[] = listRemoveFirst2andLast4.filter(number=> number%4 !==0)
+    console.log(listFilter)
+    const summe:number = listFilter.reduce((acc,curr) => acc+curr,0)
+    console.log("Die Ergebnis ist: "+ summe)
+}
 function service(n: number, s: number | null, sAny: any, u: string, b: boolean) {
 
     for (let i = 0; i <= n; i++) {
